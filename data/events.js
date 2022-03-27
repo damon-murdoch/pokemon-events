fb4076c2b8d91dd424c63ffd6224a9c0a71acdd6
Damon Murdoch <damonmurdoch1@gmail.com>
2022-03-27 16:54:25 +1000

    Moved past tournament to previous

--- data/events.js
+++ data/events.js
@@ -1,9 +1,8 @@
 const EVENTS = {
   'upcoming': [
     // Date, Venue, Format, Type (Top Cut, No Top Cut), Registration Start, Registration End (Tournament Start), Entry Fee
-    ["27/03/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/rkCBBjR9SqUSR8688'> Good Games Brisbane </a>", "VGC 2022 (Series 12)", "Swiss Rounds + Top Cut (Closed Teamsheet)", "12:00 PM", "12:30 PM", "$5.00"]
   ], 
   'previous': [
-
+    ["27/03/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/rkCBBjR9SqUSR8688'> Good Games Brisbane </a>", "VGC 2022 (Series 12)", "Swiss Rounds + Top Cut (Closed Teamsheet)", "12:00 PM", "12:30 PM", "$5.00", "Ned Pitts"]
   ]
 }
