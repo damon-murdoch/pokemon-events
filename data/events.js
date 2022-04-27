5b30a6ccfa700361768ac16c34c4c370f2b1cd2f
Damon Murdoch <Damon.Murdoch@qed.qld.gov.au>
2022-04-27 17:23:49 +1000

    Added new tournament in Beenleigh, updated standings, updated previous tournaments :)

--- data/events.js
+++ data/events.js
@@ -1,9 +1,10 @@
 const EVENTS = {
   'upcoming': [
     // Date, Venue, Format, Type (Top Cut, No Top Cut), Registration Start, Registration End (Tournament Start), Entry Fee
-    ["24/04/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/rkCBBjR9SqUSR8688'> Good Games Brisbane </a>", "VGC 2022 (Series 12)", "Swiss Rounds + Top Cut (Closed Teamsheet)", "12:00 PM", "12:30 PM", "$5.00"]
+    ["07/05/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/bynomFjfeE1oN6mz9'> Trade Binder Games </a>", "VGC 2022 (Series 12)", "Swiss Rounds + Top Cut (Closed Teamsheet)", "10:00 AM", "11:00 AM", "$10.00"]
   ], 
   'previous': [
-    ["27/03/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/rkCBBjR9SqUSR8688'> Good Games Brisbane </a>", "VGC 2022 (Series 12)", "Swiss Rounds + Top Cut (Closed Teamsheet)", "12:00 PM", "12:30 PM", "$5.00", "Ned Pitts"]
+    ["24/04/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/rkCBBjR9SqUSR8688'> Good Games Brisbane </a>", "VGC 2022 (Series 12)", "Swiss Rounds + Top Cut (Closed Teamsheet)", "12:00 PM", "12:30 PM", "$5.00", "Teagan McDonald"],
+    ["27/03/2022", "<a class='link-light' target='_blank' href='https://goo.gl/maps/rkCBBjR9SqUSR8688'> Good Games Brisbane </a>", "VGC 2022 (Series 12)", "Swiss Rounds + No Cut (Closed Teamsheet)", "12:00 PM", "12:30 PM", "$5.00", "Ned Pitts"]
   ]
 }
--- data/ladder.js
+++ data/ladder.js
@@ -1,7 +1,9 @@
 const LADDER = [
-  ["Ned Pitts", [], [1]],
-  ["Teagan Stiga", [], [2]],
-  ["Curtis Ridings", [], [3]],
-  ["Damon Murdoch", [], [4]],
-  ["Warwick Francis", [], [5]]
+  ["Ned Pitts", [6], [1]],
+  ["Teagan Stiga", [1], [2]],
+  ["Curtis Ridings", [4], [3]],
+  ["Damon Murdoch", [2], [4]],
+  ["Warwick Francis", [], [5]],
+  ["Joseph Garrett", [3], []],
+  ["Michael Herbert", [5], []]
 ];
